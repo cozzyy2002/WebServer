c610370df6947b24d24f73f34e62d9f0f423ce41
cozzyy2002 <cozzyy2002gmail.com>
2018-12-16 15:24:49 +0900

    Modify HTTP response to show HTML document.

--- scripts/HTTPServer.js
+++ scripts/HTTPServer.js
@@ -3,6 +3,7 @@
 var port = 443;
 var ip = '127.0.0.1';
 
+var util = require('util');
 var https = require('https');
 var fs = require('fs');
 var options = {
@@ -11,8 +12,12 @@ var options = {
 	cert: fs.readFileSync('../ssl/multi.test.com.crt'),
 };
 https.createServer(options, function(req,res) {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hello World\n');
+	console.log("Responding %s", req.headers.host);
+
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	var body = util.format('<html><body><h2>I am %s</h2></body></html>',
+							req.headers.host);
+	res.end(body);
 }).listen(port, ip);
 
 console.log("Server running at https://%s:%d", ip, port);
